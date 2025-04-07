bedd064a78b1dfbd815d7b2a0272fe3cb1768a38
Yazninja <yazlesean@gmail.com>
2025-04-07 12:11:11 +0300

    Add operated flights

--- Flight_System/src/Entitites/Airline.java
+++ Flight_System/src/Entitites/Airline.java
@@ -3,14 +3,26 @@ package Entitites;
 import java.util.ArrayList;
 
 public class Airline {
-	public String airlineCode;
-	public String airlineName;
-	public ArrayList<Flight> operatedFlights;
+	private String airlineCode;
+	private String airlineName;
+	private ArrayList<Flight> operatedFlights;
 
 	// Constructor
 	public Airline(String airlineCode, String airlineName) {
 		this.airlineCode = airlineCode;
 		this.airlineName = airlineName;
 		this.operatedFlights = new ArrayList<Flight>();
+	}
+
+	public String getAirlineCode() {
+		return airlineCode;
+	}
+
+	public String getAirlineName() {
+		return airlineName;
+	}
+
+	public ArrayList<Flight> getOperatedFlights() {
+		return operatedFlights;
 	}
 }
